daff56675769579ac526c1dd9949bdbcc0b5c93a
Dev Aryan Dogra <devdogra1@gmail.com>
2023-06-26 12:41:06 +0530

    Finish initial posts population in /feed

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,13 +1,13 @@
 import "./Post.css";
 
-export default function Post({ author, _id, content, weight }) {
+export default function Post({ author, _id, content, weight, title }) {
   return (
-    <div className="post" data-post-id={_id}>
-      <h3 className="post-title">{"TITLES NOT FETCHED YET"}</h3>
+    <div className="post" data-post-id={_id} data-author-id={author?._id}>
+      <h3 className="post-title">{title || "--  No Title -- "}</h3>
       <p className="post-content">{content}</p>
       <div className="post-info">
-        <p className="post-author">{author}</p>
-        <p className="post-weight">{weight}</p>
+        <p className="post-author">{author?.username || "unknown"}</p>
+        <p className="post-weight">{weight || "unknown"}</p>
       </div>
     </div>
   );
