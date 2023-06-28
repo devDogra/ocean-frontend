fd153aaefce9d4d2836b1290cb185f66b8ea0934
Dev Aryan Dogra <devdogra1@gmail.com>
2023-06-28 15:55:20 +0530

    Redirect to /login upon succesful registration

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,18 +1,27 @@
 import "./RegisterForm.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 import { apiURL } from "../../fakeEnvVars";
 
 export default function RegisterForm() {
+  let navigate = useNavigate();
+
   async function handleSubmit(event) {
     event.preventDefault();
     const form = event.target;
     const formData = new FormData(form);
     const formDataJson = Object.fromEntries(formData.entries());
 
-    const response = await axios.post(apiURL + "/register", formDataJson);
-    console.log(response);
+    try {
+      const response = await axios.post(apiURL + "/register", formDataJson);
+      console.log(response);
+      alert("Account created succesfully");
+      navigate("/login"); 
+    } catch(err) {
+      alert(err.message);
+    }
+
   }
   return (
     <form action="#" className="register-form" onSubmit={handleSubmit}>
