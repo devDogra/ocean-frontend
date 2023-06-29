ccf30cd835d72f39c21d95a9c9fb7c20673ec7a6
Dev Aryan Dogra <devdogra1@gmail.com>
2023-06-30 01:20:35 +0530

    Add rudimentary up/downvote buttons on Post

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -7,7 +7,12 @@ export default function Post({ author, _id, content, weight, title }) {
       <p className="post-content">{content}</p>
       <div className="post-info">
         <p className="post-author">{author?.username || "unknown"}</p>
-        <p className="post-weight">{weight || "unknown"}</p>
+        <div className="post-weight-controls">
+          <button className="post-vote-btn">⬆️</button>
+          <p className="post-weight">{weight || "unknown"}</p>
+          <button className="post-vote-btn">⬇️</button>
+        </div>
+
       </div>
     </div>
   );
