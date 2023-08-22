e8c651c39426e15d11bb55c11edb1d46dfa3a48b
Dev Aryan Dogra <devdogra1@gmail.com>
2023-08-22 22:13:20 +0530

    Create extractHashtags function in NewPostForm.jsx

--- src/components/NewPostForm/NewPostForm.jsx
+++ src/components/NewPostForm/NewPostForm.jsx
@@ -6,8 +6,12 @@ import axios from "axios";
 import getAxiosRequestConfig from "../../utils/functions/getAxiosRequestConfig";
 import getLoggedInUserIdFromJWT from "../../utils/functions/getLoggedInUserIdFromJWT";
 import getFormJSONData from "../../utils/functions/getFormJSONData";
+// import { debounce } from "lodash";
+import debounce from "lodash/debounce"; 
+import { throttle } from "lodash";
 
 export default function NewPostForm() {
+
     async function handleSubmit(event) {
         event.preventDefault(); 
 
@@ -18,14 +22,23 @@ export default function NewPostForm() {
         let form = event.target; 
         let formDataJson = getFormJSONData(form); 
         formDataJson.author = loggedInUserId; 
+        console.log({formDataJson}); 
+        console.log(extractHashtags(formDataJson.content)); 
 
         try {
-            await axios.post(apiURL + '/posts', formDataJson, config);
+            // await axios.post(apiURL + '/posts', formDataJson, config);
             alert("Post created"); 
         } catch(err) {
             alert("ERROR: Could not create post"); 
         }
     }
+    
+    function extractHashtags(content) {
+        const hashtagRegex = new RegExp("#+[a-zA-Z0-9(_)]{1,}", "g");
+        const tags = content.match(hashtagRegex);
+        return tags; 
+    }
+
 
     return (
         <form id="new-post-form" action="#"  onSubmit={handleSubmit}>
